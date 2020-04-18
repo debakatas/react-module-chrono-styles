1d9d8fd19c9a8d6a0127181b861138dbf1d45e37
Nicolás M. Pardo <nicolasm@nikodermus.media>
2020-04-17 21:38:16 -0500

    feat(chrono): basic working timer

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import styled from 'styled-components';
 import styles from './style/App.module.css';
 
@@ -6,13 +6,88 @@ const StyledDiv = styled.div`
     transform: rotate(5deg);
 `;
 
-const App = () => (
-    <StyledDiv
-        className={`${styles.big} globalClassname`}
-        style={{ textTransform: 'uppercase' }}
-    >
-        Chrono
-    </StyledDiv>
-);
+const App = () => {
+    const [runTime, setRunTime] = useState(0);
+    const [time, setTime] = useState(Date.now());
+    const [initialTime, setInitialTime] = useState(
+        Date.now()
+    );
+    const [lap, setLap] = useState([]);
+    const [go, setGo] = useState(false);
+
+    const updateTime = () => {
+        setTime(Date.now());
+    };
+
+    const takeLap = (newLap) => {
+        setLap((prevLaps) => [...prevLaps, newLap]);
+    };
+
+    useEffect(() => {
+        if (go) {
+            setTimeout(() => {
+                updateTime();
+            }, 100);
+        }
+    }, [go, time]);
+
+    const currentTime = go
+        ? runTime + (time - initialTime) / 1000
+        : runTime;
+
+    return (
+        <>
+            <StyledDiv
+                className={`${styles.big} globalClassname`}
+                style={{ textTransform: 'uppercase' }}
+            >
+                {currentTime.toFixed(2)}
+            </StyledDiv>
+            <button
+                type="button"
+                onClick={() => {
+                    setGo((b) => {
+                        const newTime = Date.now();
+                        if (b) {
+                            setRunTime(currentTime);
+                        } else {
+                            setInitialTime(newTime);
+                            setTime(newTime);
+                        }
+
+                        return !b;
+                    });
+                }}
+            >
+                {go ? 'Parar' : 'comenzar'}
+            </button>
+            <button
+                type="button"
+                onClick={() => takeLap(currentTime)}
+            >
+                take lap
+            </button>
+            <button
+                type="button"
+                onClick={() => {
+                    const newTime = Date.now();
+                    setTime(newTime);
+                    setRunTime(0);
+                    setInitialTime(newTime);
+                    setGo(false);
+                    setLap([]);
+                }}
+            >
+                restart
+            </button>
+            {lap.map((l) => (
+                <div style={{ marginRight: 20 }}>
+                    {l.toFixed(2)}
+                    <br />
+                </div>
+            ))}
+        </>
+    );
+};
 
 export default App;
